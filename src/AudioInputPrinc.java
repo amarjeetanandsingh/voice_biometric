5d9a19fcc702b343681260b035a721014c41912a
Amarjeet Anand <amarjeetanandsingh@gmail.com>
2016-11-05 12:04:54 +0530

    new working files are added to repo

---
+++ src/AudioInputPrinc.java
@@ -0,0 +1,118 @@
+import java.awt.*;
+import javax.sound.sampled.*;
+import java.nio.*;
+
+class AudioInputPrinc{
+	
+	TargetDataLine microphone;
+
+	final int audioFrames 	= 16384;	
+	final float sampleRate 	= 44100.0f;
+	final int bitsPerRecord = 16; 
+	final int channels 		= 1; 
+	final boolean bigEndian = true;
+	final boolean signed 	= true;
+
+	byte[] byteData;		// length = audioFrames*2;
+	double[] doubleData;	// length = audioFrames only reals needed for apache lib.
+
+	AudioFormat format;
+
+	public AudioInputPrinc(){
+		byteData = new byte[audioFrames*2];
+		// doubleData = new double[audioFrames*2]; 	// real & imaginary
+		doubleData = new double[audioFrames];		// only real for apache
+		System.out.println("Microphone initialising...");
+		format = new AudioFormat(sampleRate, bitsPerRecord, channels, signed, bigEndian);
+	 	DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+		// **
+		if(!AudioSystem.isLineSupported(info)){
+			System.err.println("AudioSystem is not LineSupported(info)");
+			System.exit(1);
+		}
+		try{
+			microphone = (TargetDataLine)AudioSystem.getLine(info);
+			microphone.open(format); 
+			System.out.println("Microphone opened with format : "+format.toString());
+
+			microphone.start();
+
+		}catch(LineUnavailableException ex){
+			System.out.println("Line Unavailable Exception");
+			System.out.println("Microphone failed to start. "+ex.getMessage());
+			System.exit(1);
+		}
+	}
+
+	public int readPCM(){
+		int numBytesRead = microphone.read(byteData, 0, byteData.length);
+		if(numBytesRead != byteData.length){
+			System.out.println("warning: Less bytes read than buffer");
+			System.exit(1);
+		}
+		return numBytesRead;
+	}
+
+	public void byteToDouble(){
+		ByteBuffer buf = ByteBuffer.wrap(byteData);
+		buf.order(ByteOrder.BIG_ENDIAN);
+		int i =0;
+		while(buf.remaining()>2){
+			short s = buf.getShort();
+			doubleData[i] = (new Short(s)).doubleValue();
+			++i;
+		}
+	}
+
+	public void findFrequency(){
+		double frequency;
+		Complex [] complexRaw = new Complex[doubleData.length];
+		for (int i=0; i<doubleData.length; i++) {
+			complexRaw[i] = new Complex(doubleData[i],0);
+		}
+
+		Complex[] complx = FFT.fft(complexRaw);
+		double real;
+		double im; 
+		double mag[] = new double[complx.length];
+		for (int i=0; i<complx.length; i++) {
+			real = complx[i].re();
+			im = complx[i].im();
+			mag[i] = Math.sqrt((real*real)+(im*im));
+		}
+		double peak = -1;
+		int index = -1;
+		for (int i=0; i<complx.length; i++) {
+			if(peak<mag[i]){
+				index = i;
+				peak = mag[i];
+			}
+		}
+		frequency = (sampleRate*index)/audioFrames;
+		System.out.println(index+"\t"+frequency);
+
+	}
+
+	public void printFrequency(){
+		for (int i=0; i<audioFrames/4; i++) {
+			System.out.println("bin "+i+", Frequency : "+(sampleRate*i)/audioFrames);
+		}
+	}
+
+	public static void main(String[] args){
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		tk.beep();
+		
+		AudioInputPrinc ai = new AudioInputPrinc();
+
+		int turns = 100;
+		System.out.println("BIN 		Frequency 		");
+		long startTime = System.currentTimeMillis();
+			ai.readPCM();
+			ai.byteToDouble();
+			ai.findFrequency();
+			// ai.printFrequency();
+		System.out.println("Time taken : "+((System.currentTimeMillis()-startTime)/1000.00) +" ms");
+	}
+
+}
